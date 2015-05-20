45d6dc9cb984753243f6457022cd6e53f42497f9
Chris Sloop <yaphetf@gmail.com>
2015-05-20 09:57:17 -0400

    Refactor with singular *args parameter

--- solution/array_each_count_solution.rb
+++ solution/array_each_count_solution.rb
@@ -1,10 +1,10 @@
 class Array
-  def each_count method = nil, *args
-    raise ArgumentError, "block supplied with arguments" if block_given? && !method.nil?
+  def each_count *args
+    raise ArgumentError, "block supplied with arguments" if block_given? && !args.empty?
 
     memo = {}
     self.each do |item|
-      item = item.send(method, *args) if !method.nil?
+      item = item.send(*args) if not args.empty?
       item = yield(item) if block_given?
       memo.include?(item) ? memo[item] += 1 : memo[item] = 1
     end
