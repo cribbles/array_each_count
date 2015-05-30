bb23bdd018c361719f3fe9761658a2e44a4dadf9
Chris Sloop <yaphetf@gmail.com>
2015-05-29 22:43:26 -0400

    Refactor with each_with_object
    Also change 'if not' to 'unless' to be a little more ideomatic :)

--- solution/array_each_count_solution.rb
+++ solution/array_each_count_solution.rb
@@ -2,12 +2,11 @@ class Array
   def each_count *args
     raise ArgumentError, "block supplied with arguments" if block_given? && !args.empty?
 
-    memo = {}
-    self.each do |item|
-      item = item.send(*args) if not args.empty?
+    each_with_object(Hash.new) do |item, hash|
+      item = item.send(*args) unless args.empty?
       item = yield(item) if block_given?
-      memo.include?(item) ? memo[item] += 1 : memo[item] = 1
+      hash[item] ||= 0
+      hash[item] += 1
     end
-    memo
   end
 end
